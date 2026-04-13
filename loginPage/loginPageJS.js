0fbccfe57b8a51e6a1e42ce2835d6c3ad8d258ae
SebCas07 <tiancas07@gmail.com>
2026-04-13 17:48:32 -0500

    Completed JS for loginPageJS.js.  Fixed issue with 'continueBtn' not working by catching a misspelled line of code in loginPageJS.js and renaming the <button> tag id to 'continue-btn' in loginPage_campusLifeAPP.html.

--- loginPage/loginPageJS.js
+++ loginPage/loginPageJS.js
@@ -3,7 +3,7 @@
 // window.location.href = "../home/homePage.html"
 // This tells the browser to load a new page - no show/hide needed
 
-const continueBtn = document.getElemenetById('continue-btn'); 
+const continueBtn = document.getElementById('continue-btn'); 
 const usernameInput = document.getElementById('username'); 
 const passwordInput = document.getElementById('password'); 
 const errorMsg = document.getElementById('login-error'); 
@@ -23,4 +23,30 @@ function handleLogin() {
     }
 
     errorMsg.classList.add('d-none'); 
+
+    // Storing the username in sessionStorage so other pages can use it.
+    // sessionStorage persists while the browsert tab is open 
+    //and clears automatically when the tab is closed.
+
+    //later, the home page can read it with : 
+        //const user = sessionStorage.getItem('campusUser'); 
+    
+    sessionStorage.setItem('campusUser', username); 
+
+    // navigate to the home page
+    window.location.href= '../home/homePage.html';
+
 }
+
+//  EVENT LISTENERS
+
+continueBtn.addEventListener('click', handleLogin); 
+
+// allow pressing Enter fomr either input field to submit
+usernameInput.addEventListener('keydown', (e)=> { 
+    if (e.key === 'Enter') handleLogin(); 
+});
+
+passwordInput.addEventListener('keydown', (e)=> {
+    if(e.key === 'Enter') handleLogin(); 
+});
